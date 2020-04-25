9a01fba46ca6a77bbe59d7b160dce50d694c8404
Angad Singh <Angad@Angads-Macbook-Pro.local>
2020-04-25 18:42:51 -0500

    Added time functionality for records

--- app/src/main/java/com/example/fitnotes/DropDownActivity.java
+++ app/src/main/java/com/example/fitnotes/DropDownActivity.java
@@ -1,7 +1,11 @@
 package com.example.fitnotes;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +15,8 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Date;
+
 public class DropDownActivity extends AppCompatActivity {
 
     int sets, reps, oneRM;
@@ -19,10 +25,11 @@ public class DropDownActivity extends AppCompatActivity {
     EditText onermInput;
 
     Button saveBttn;
+    Record record;
 
     //Firebase setup
     FirebaseDatabase mydatabase;
-    DatabaseReference records;
+    DatabaseReference recordsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +44,12 @@ public class DropDownActivity extends AppCompatActivity {
 
         //Firebase connect
         mydatabase = FirebaseDatabase.getInstance();
-        records = mydatabase.getReference("Record");
+        recordsRef = mydatabase.getReference("Record");
+
+        record = new Record();
 
         saveBttn.setOnClickListener(new View.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v){
                 sets = Integer.valueOf(setsInput.getText().toString());
@@ -49,6 +59,22 @@ public class DropDownActivity extends AppCompatActivity {
                 showToast("Sets entered: " + String.valueOf(sets));
                 showToast("Reps entered: " + String.valueOf(reps));
                 showToast("1RM entered: " + String.valueOf(oneRM));
+
+                sets = Integer.parseInt(setsInput.getText().toString().trim());
+                reps = Integer.parseInt(repsInput.getText().toString().trim());
+                oneRM = Integer.parseInt(onermInput.getText().toString().trim());
+
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = new Date();
+                String strDate = dateFormat.format(date).toString();
+                //myRef.child("datetime").setValue(strDate);
+
+                record.setSets(sets);
+                record.setReps(reps);
+                record.setOneRm(oneRM);
+                record.setDate(strDate);
+
+                recordsRef.push().setValue(record);
             }
 
         });
--- app/src/main/java/com/example/fitnotes/Record.java
+++ app/src/main/java/com/example/fitnotes/Record.java
@@ -1,10 +1,13 @@
 package com.example.fitnotes;
 
+import java.text.DateFormat;
+
 public class Record {
 
     private Integer sets;
     private Integer reps;
     private Integer oneRm;
+    String date;
 
     public Record() {
     }
@@ -31,5 +34,13 @@ public class Record {
 
     public void setOneRm(Integer oneRm) {
         this.oneRm = oneRm;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
